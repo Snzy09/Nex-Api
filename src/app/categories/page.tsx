84f4e783405fb13068087aaed2b33065ce0fcbaf
S A N Z - N E W B I E <Ikhsancandra721@gmail.com>
2025-11-09 21:11:18 +0700

    Update page.tsx

--- src/app/categories/page.tsx
+++ src/app/categories/page.tsx
@@ -19,7 +19,6 @@ const categoryIcons: { [key: string]: React.ElementType } = {
     random: ImageIcon,
     news: Newspaper,
     anime: Film,
-    manga: BookOpen,
 };
 
 export default function DashboardPage() {
