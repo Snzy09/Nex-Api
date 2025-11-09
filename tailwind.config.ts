ac8653891e38e9a566f8cea809683294abf52f5d
S A N Z - N E W B I E <Ikhsancandra721@gmail.com>
2025-11-09 19:55:12 +0700

    UP

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,13 +10,7 @@ export default {
   theme: {
     container: {
       center: true,
-      padding: {
-        DEFAULT: "1rem",
-        sm: "2rem",
-        lg: "2rem",
-        xl: "2rem",
-        "2xl": "2rem",
-      },
+      padding: "2rem",
       screens: {
         "2xl": "1400px",
       },
