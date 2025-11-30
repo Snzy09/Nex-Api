dd444a01387a5ac9b97b3c8822f1d30c91bc6f48
S A N Z - N E W B I E <Ikhsancandra721@gmail.com>
2025-11-30 13:47:10 +0000

    up

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import './globals.css';
 import { ThemeProvider } from '@/components/theme-provider';
 import { Toaster } from "@/components/ui/toaster"
+import VercelAnalytics from '@/components/vercel-analytics'
 
 export const metadata: Metadata = {
   title: 'Nex Api 〽️',
@@ -28,6 +29,7 @@ export default function RootLayout({
         >
           {children}
           <Toaster />
+          <VercelAnalytics />
         </ThemeProvider>
       </body>
     </html>
